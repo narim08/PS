bdb7dcf3e15ae30117eafba706f2ae49fdc41697
Narim Park <155282079+narim08@users.noreply.github.com>
2024-01-31 15:51:40 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t, r;
+    string s;
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        cin >> r >> s;
+        for (int j = 0; j < s.length(); j++) {
+            for (int k = 0; k < r; k++) {
+                cout << s[j];
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
