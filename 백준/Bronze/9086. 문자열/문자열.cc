4b2b65158fe7f8b7ef021da33d3dd2c43878333d
Narim Park <155282079+narim08@users.noreply.github.com>
2024-01-31 15:50:50 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    string str;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> str;
+        cout << str[0] << str[str.length() - 1] << "\n";
+    }
+
+    return 0;
+}
