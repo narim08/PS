a218193ad8307aee934e399d482cf5df792d43bc
Narim Park <155282079+narim08@users.noreply.github.com>
2024-01-31 15:46:52 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=1, b=1;
+    while (1) {
+        cin >> a >> b;
+        if (a != 0 && b != 0) {
+            cout << a + b << "\n";
+        }
+        else break;
+    }
+    return 0;
+}
